dc91aa94aaa46070c47e3a4a9e1666c7da3c24da
jos4211 <q1w2e3123@naver.com>
2021-03-02 11:12:02 +0900

    netlify와 연결중

--- board_front/pages/index.tsx
+++ board_front/pages/index.tsx
@@ -8,34 +8,46 @@ import MainContainer from "../components/mainContainer";
 import Board from "../components/board";
 
 
-export const getStaticProps: GetServerSideProps = async (context) => {
+// export const getStaticProps: GetServerSideProps = async (context) => {
 
-  const apiUrl:string = "https://boardnextype.herokuapp.com/api/board/boardList";
+//   const apiUrl:string = "https://boardnextype.herokuapp.com/api/board/boardList";
 
-  try{
-    const res: Response  = await fetch(apiUrl);
+//   try{
+//     const res: Response  = await fetch(apiUrl);
     
-    console.log("boardList",res);
-    
-    const boardList = await res.json();
+//     console.log("boardList",res);
+
+//     const boardList = await res.json();
     
   
     
-    return {
-      props: { 
-        data: boardList,
-      },
-    };
-  }catch(e){
-    console.log(e);
-    return {
-      props: { 
-        data: null,
-      },
-    };
-  }
+//     return {
+//       props: { 
+//         data: boardList,
+//       },
+//     };
+//   }catch(e){
+//     console.log(e);
+//     return {
+//       props: { 
+//         data: null,
+//       },
+//     };
+//   }
     
-  }
+//   }
+
+export async function getStaticPaths() {
+
+  const apiUrl:string = "https://boardnextype.herokuapp.com/api/board/boardList";
+  
+  const res: Response = await fetch(apiUrl);
+
+  const boardList = await res.json();
+
+  return {params:{data:boardList},fallback: false };
+}
+
 
 
 export default function Home({data}) {
